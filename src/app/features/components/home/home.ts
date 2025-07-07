53cb7cfaeff30b79b762cf2b770d3a1e8dc13c3c
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-07-07 10:47:32 +0300

    update home screen

--- src/app/features/components/home/home.ts
+++ src/app/features/components/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.scss'
 })
 export class Home {
+  constructor(private router: Router) {}
 
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
