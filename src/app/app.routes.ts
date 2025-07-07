824a10c927161bcc46fe0d2ffefaf871497d82ed
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-07-07 09:35:55 +0300

    create project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./features/components/home/home').then(c => c.Home)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/components/login/login').then(c => c.Login)
+  },
+  {
+    path: 'todos',
+    loadComponent: () => import('./features/components/todos/todos').then(c => c.Todos)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
